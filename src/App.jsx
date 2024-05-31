c6970f17190140196f06437c494c1bdbd83bd861
student-Swati <ruchiswati1631@gmail.com>
2024-05-31 15:29:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import JewellerySection from './Components/JewellerySection';
+import Navigation from './Components/Navigation';
+const App = () => {// it is a component
+    return(
+    <div>
+       <Navigation/>
+       <JewellerySection/>
+       
+    </div>
+    );
+};
+
+export default App; 
---
+++ src/Components/JewellerySection.jsx
@@ -0,0 +1,32 @@
+const JewellerySection = () => {
+   return <main className="jewellery container">
+    <div className="Jewellery-content">
+     <h1>
+       THE RIGHT PIECE OF JEWELLERY CAN TRANSFORM AN ORDINARY OUTFIT INTO SOMETHING EXTRAORDINARY.
+     </h1>
+     <p>
+
+    </p>
+
+      <div className="jewel- btn">
+        <button > Shop Now </button>
+        <button> Category </button>
+      </div>
+      <div className="shop">
+        <p>Also Available On</p>
+        <div className="Brand-Available">
+            <img src="/images/amazon.jpg" alt="amazon-logo" />
+            <img src="/images/giva.jpg" alt="giva-logo" />
+        </div>
+      </div>
+   </div>
+    <div className="Jewellery-img">
+      <img src="/images/necklace.jpg" alt="necklace set" />
+
+    </div>
+
+   </main>
+
+
+};
+export default JewellerySection;
